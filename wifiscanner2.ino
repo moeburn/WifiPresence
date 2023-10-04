2b9dbf48f315d59df80d9eb04be05958e823b3d5
moeburn <moeburn@gmail.com>
2023-10-03 20:55:44 -0400

    add Blynk

--- wifiscanner2.ino
+++ wifiscanner2.ino
@@ -17,9 +17,14 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncElegantOTA.h>
+#include <BlynkSimpleEsp32.h>
+char auth[] = "5D3OmcWGz1X1QeFZrl_HS0E6EgOaQA7y";  //BLYNK
 
 #define LIGHT_TIMER 60 //seconds for lights to remain on after detecting wifi signal
-#define RSSI_MIN -60 //minimum RSSI signal strength to turn lights on (negative values)
+#define RSSI_MIN -55 //minimum RSSI signal strength to turn lights on (negative values)
+
+#define BLYNK_PRINT Serial // Defines the object that is used for printing
+#define BLYNK_DEBUG // Optional, this enables more detailed prints
 
 AsyncWebServer server(80);
 
@@ -30,6 +35,7 @@ AsyncWebServer server(80);
 CRGB leds[NUM_LEDS];
 
 Approximate approx;
+WiFiClient wifiClient;
 
  SH1106Wire display(0x3c, SDA, SCL);   
 
@@ -43,6 +49,11 @@ bool ledState = LOW;
 long ledToggleAtMs = 0;
 int ledToggleIntervalMs = 0;
 
+int lightsTime = 0;
+int rssistrength;
+unsigned long timermillis, millisTemp;
+CRGB color1 = CRGB(255, 100, 0);
+
 void setup() {
   display.init();
   display.setFont(ArialMT_Plain_16);
@@ -52,8 +63,23 @@ void setup() {
   if (approx.init("mikesnet", "springchicken")) {
     approx.setActiveDeviceFilter("64:89:F1:AF:94:85");
     approx.setActiveDeviceHandler(onActiveDevice);
-    approx.begin();
+    approx.begin([]() {
+  Blynk.config(auth, IPAddress(192, 168, 50, 197), 8080);
+  
+    });
   }
+
+      int x; 
+      while (WiFi.status() != WL_CONNECTED) {
+    x+=3;
+    if (x > 128){x=0;}
+    
+    display.drawString(x, 0, ".");
+    display.display();
+    delay(250);
+  }
+approx.connectWiFi();
+Blynk.connect();
     Serial.println("");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -65,16 +91,29 @@ void setup() {
   AsyncElegantOTA.begin(&server);    // Start ElegantOTA
   server.begin();
   Serial.println("HTTP server started");
+    
+
+     //   approx.onceWifiStatus(WL_CONNECTED, [](String payload) {
+ 
+
+
+  //  }, json);
+timermillis = millis();
+
 }
 
-int lightsTime = 0;
-int rssistrength;
-unsigned long timermillis;
-CRGB color1 = CRGB(255, 100, 0);
+bool newdata = false;
 
 void loop() {
   //delay(10);
   approx.loop();
+    Blynk.run();
+          if ((millis() - millisTemp >= 1000) && newdata)
+      {
+        millisTemp = millis();
+        Blynk.virtualWrite(V1, rssistrength);
+        newdata = false;
+      }
     display.clear();
     String rssistring = "RSSI: " + String(rssistrength);
     display.drawString(0, 0, rssistring);
@@ -84,13 +123,14 @@ void loop() {
   if ((millis() - timermillis) < (LIGHT_TIMER * 1000)){
           fill_solid(leds, NUM_LEDS, color1);
   FastLED.show();
-      String timestring = "Time: " + String(millis() - timermillis);
+      String timestring = "Time: " + String((LIGHT_TIMER * 1000) - (millis() - timermillis));
     display.drawString(0, 16, timestring);
   }
   else {
           fill_solid( leds, NUM_LEDS, CRGB(0,0,0));
   FastLED.show();
   }
+
 
   /*if (lightsTime > 0){
     lightsTime--;
@@ -112,11 +152,17 @@ void loop() {
 
 void onActiveDevice(Device *device, Approximate::DeviceEvent event) {
   if(event == Approximate::SEND) {  
-    
+        String json = "{\"" + device->getMacAddressAsString() + "\":\"" + Approximate::toString(event) + "\"}";
     //ledToggleIntervalMs = map(device->getRSSI(), -100, 0, 1000, 0);
     Serial.println(device->getRSSI());
     rssistrength = device->getRSSI();
-    if (rssistrength > RSSI_MIN){lightsTime = 3000;
+    if (rssistrength > RSSI_MIN){
     timermillis = millis();}
+    newdata = true;
+       
+  /* approx.onceWifiStatus(WL_CONNECTED, [](String payload) {
+      Blynk.connect();
+    }, json);
+    approx.connectWiFi();*/
   }
 }
